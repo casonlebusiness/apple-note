fb9765c608bc5b454cb02a9e3e5f0c39b34bbe2b
cason <casonlebusiness@gmail.com>
2022-11-22 09:46:51 +0700

    Update env variables

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,12 +6,12 @@ import 'firebase/storage';
 export const firebaseConfig = {
   // your config
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  authDomain: "apple-note.firebaseapp.com",
+  projectId: "apple-note",
+  storageBucket: "apple-note.appspot.com",
+  messagingSenderId: "262747200533",
+  appId: "1:262747200533:web:10e3f34cacb68f3dc0021c",
+  measurementId: "G-TN80MJTQGDgit"
 };
 
 if (!firebase.apps.length) {
