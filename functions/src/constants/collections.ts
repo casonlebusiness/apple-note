3c938383252d2fdd9056043109e45a9f0f8fb25c
cason <casonlebusiness@gmail.com>
2022-11-22 09:31:23 +0700

    Remove unused folder

--- functions/src/constants/collections.ts
+++
@@ -1,7 +0,0 @@
-export enum COLLECTION_NAMES {
-  BIN_SELECTION_POSTCODE_CONFIG = "bin-selection-postcode-config",
-  ANALYTICS = "analytics",
-  PRODUCTS = "products",
-  REPORTS = "reports",
-  USER_BIN_SELECTIONS = "user-bin-selections"
-}
--- functions/src/migrations/index.ts
+++
@@ -1,6 +0,0 @@
-import { sampleMigration } from "./sample-migration"
-
-// eslint-disable-next-line import/no-anonymous-default-export
-export const migrations = {
-  sampleMigration
-}
